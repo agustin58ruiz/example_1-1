010dd5fb550af4e222270c3275c75ffd1271c80e
Agustín Ruiz <aeruiz@fi.uba.ar>
2024-08-23 00:10:34 +0000

    "Agus and Bri added changes"

--- main.cpp
+++ main.cpp
@@ -1,23 +1,41 @@
 #include "mbed.h"
 #include "arm_book_lib.h"
 
+#define BLINKING_TIME_GAS_ALARM 1000
+#define BLINKING_TIME_OVER_TEMP_ALARM_ 500
+#define BLINKING_TIME_GAS_AND_OVER_TEMP_ALARM 100
+
 int main()
 {
     DigitalIn gasDetector(D2);
 
     DigitalOut alarmLed(LED1);
+    DigitalOut alarmLed1(LED2);
+    DigitalOut alarmLed2(LED3);
+    printf("%s\n", "HelloWorld");
+    
 
     gasDetector.mode(PullDown);
 
     alarmLed = OFF;
-    
-    while (true) {
-        if ( gasDetector == ON ) {
-            alarmLed = ON;
-        }
-        
-        if ( gasDetector == OFF ) {
-            alarmLed = OFF;
-        }
+    //
+    while(true){
+        printf("%s\n", "HelloWorld");
+        alarmLed=gasDetector;
+        delay( BLINKING_TIME_GAS_AND_OVER_TEMP_ALARM );
+        alarmLed1=gasDetector;
+        delay( BLINKING_TIME_GAS_ALARM );
+        alarmLed2=gasDetector;
+        delay(  BLINKING_TIME_GAS_AND_OVER_TEMP_ALARM );
+
     }
+    //while (true) {
+    //    if ( gasDetector == ON ) {
+    //        alarmLed = ON;
+    //    }
+    //    
+    //    if ( gasDetector == OFF ) {
+    //        alarmLed = OFF;
+    //    }
+    //}
 }
